b86b26e833fb7f01098823e774c9818d5642f299
Mert <yigittopm@hotmail.com>
2020-12-24 22:10:28 +0300

    font awesome added

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="card text-white bg-primary col-md-3" >
+<div class="col-md-4 col-sm-6 mb-3">
+  <div class="card text-white bg-dark md-4">
     <img class="card-img-top" src="../img/macbook.png">
     <div class="card-body">
       <p class="card-title">{{ product.name }}</p>
       <p class="card-text">
         <strong>
-            Price: {{ product.price }}€
+            Price: {{ product.price }} <i class="fas fa-dollar-sign"></i>
         </strong>
       </p>
       
     </div>
-    <button class="btn btn-success">Buy</button>
+    <button class="btn btn-warning"><i class="fas fa-shopping-cart"></i> Buy</button>
   </div>
+</div>
+  
 </template>
 
 <script>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="col-md-9">
     <h2>{{ productTitle }}</h2>
-      <div class="card-list">
-        <Product
-          v-for="product in products"
-          :key="product.id"
-          :product="product"
-         ></Product>
-      </div>
+    <div class="row">
+      <Product
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      ></Product>
+    </div>
   </div>
 </template>
 
@@ -20,12 +20,12 @@ export default {
     return {
       productTitle: "Products",
       products: [
-        { id: 1, category:"TV", name: "LG 55", price: 7000 },
-        { id: 2, category:"Laptop", name: "Macbook Air 2020", price: 7000 },
-        { id: 3, category:"Laptop", name: "Lenovo P330", price: 9000 },
-        { id: 4, category:"TV", name: "Samsung Curved", price: 4000 },
-        { id: 5, category:"Phone", name: "iPhone XS", price: 16000 },
-        { id: 6, category:"Phone", name: "Samsung S20", price: 12000 },
+        { id: 1, category: "TV", name: "LG 55", price: 7000 },
+        { id: 2, category: "Laptop", name: "Macbook Air 2020", price: 7000 },
+        { id: 3, category: "Laptop", name: "Lenovo P330", price: 9000 },
+        { id: 4, category: "TV", name: "Samsung Curved", price: 4000 },
+        { id: 5, category: "Phone", name: "iPhone XS", price: 16000 },
+        { id: 6, category: "Phone", name: "Samsung S20", price: 12000 },
       ],
     };
   },
