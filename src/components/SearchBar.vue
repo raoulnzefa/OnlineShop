408db4f387e62dc9ac657fa4bdb727e5a7f76f85
Mert <yigittopm@hotmail.com>
2020-12-25 19:11:57 +0300

    update

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="input-group mt-3">
     <input class="form-control col-md-10" type="text" placeholder="Search All">
-    <button class="form-control btn btn-primary col-md-2">Bul</button>
+    <button class="form-control btn btn-primary col-md-2">Searc</button>
 </div>
     
 </template>
