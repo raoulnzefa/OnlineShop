2415e95e8dd5d77dfa5fe306ff6cc499ce13f559
Mert <yigittopm@hotmail.com>
2020-12-29 19:12:04 +0300

    added vue-resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import VueResource from "vue-resource";
 
+Vue.use(VueResource);
 export const eventBus = new Vue();
 
 new Vue({
